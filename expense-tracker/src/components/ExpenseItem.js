98bf46614a84368f127b47b81e798b5949abfb4c
John Wells <molekewl2017@gmail.com>
2021-08-16 23:40:50 -0700

    add logic to expense component

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem({date, title, amount}) {
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+
   return (
     <div className='expense-item'>
-      <div>{date}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div>
         <h2 className='expense-item__description'>{title}</h2>
       </div>
