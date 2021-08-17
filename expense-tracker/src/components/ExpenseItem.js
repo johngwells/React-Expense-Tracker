101d0fb06815d25b14fae6027a1935b4095bdca6
John Wells <molekewl2017@gmail.com>
2021-08-16 23:26:31 -0700

    passing props and displaying dynamic data

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem({date, title, amount}) {
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{date}</div>
       <div>
-        <h2 className='expense-item__description'>Title</h2>
+        <h2 className='expense-item__description'>{title}</h2>
       </div>
-      <div className='expense-item__price'>Amount</div>
+      <div className='expense-item__price'>${amount}</div>
     </div>
   );
 }
