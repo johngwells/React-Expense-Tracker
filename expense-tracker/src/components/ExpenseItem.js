f0ea01631d63e1ab8145b68be67f895368a53382
John Wells <molekewl2017@gmail.com>
2021-08-17 01:55:51 -0700

    split code from app.js into a new component | clean code from duplication using Card as a wrapper

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem({date, title, amount}) {
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={date}/>
       <div>
         <h2 className='expense-item__description'>{title}</h2>
       </div>
       <div className='expense-item__price'>${amount}</div>
-    </div>
+    </Card>
   );
 }
 
