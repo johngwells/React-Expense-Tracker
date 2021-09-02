5fe69e88a7ce952d4879a12a2befdc97c8bb899a
John Wells <molekewl2017@gmail.com>
2021-09-01 20:41:12 -0700

    setting up state

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,51 @@
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState("");
+  const [expense, setExpense] = useState("");
+  const [date, setDate] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const expenseChangeHandler = (e) => {
+    setExpense(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  };
+
   return (
     <form>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
-          <input type='text' />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={expenseChangeHandler}
+          />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
